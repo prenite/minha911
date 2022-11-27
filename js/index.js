c5efb822b5a2aa9ed9bc9ba9e6cc87df46863eb5
prenite <117414705+prenite@users.noreply.github.com>
2022-11-27 23:01:51 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 $(document).ready(function () {
 
     //마우스오버시 내비 글자 색깔 변경
-    $('.nav a').mouseover(function(){
-        $(this).css({color:'orangered'})
+    $('.nav a').mouseover(function () {
+        $(this).css({ color: 'orangered' })
     })
-    $('.nav a').mouseleave(function(){
-        $(this).css({color:'white'})
+    $('.nav a').mouseleave(function () {
+        $(this).css({ color: 'white' })
     })
 
 
-    //메인이미지 
+    //이미지 아래에서 나타남
 
-   
+    $('.img_box').delay(1000).stop().animate({ top: '-150', opacity: 1 });
+    
+
 
 
     //메인 글자 서서히 나타남
     $('#main h2').fadeIn(2000, 'linear')
 
-    $('#main p').fadeIn(3000, 'linear')
+    $('#main p').fadeIn(1000, 'linear')
 
-    $('#main h3').fadeIn(4000, 'linear')
+    $('#main h3').fadeIn(3000, 'linear')
 
 
 });
